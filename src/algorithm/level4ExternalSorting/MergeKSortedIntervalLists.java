839e050e7b999b1ac4ba0e31b8ea8061807f3c3e
liliccc <xianj.chen@qq.com>
2022-01-19 18:15:33 +0800

    1.19

---
+++ src/algorithm/level4ExternalSorting/MergeKSortedIntervalLists.java
@@ -0,0 +1,22 @@
+package algorithm.level4ExternalSorting;
+
+import java.util.List;
+
+public class MergeKSortedIntervalLists {
+    /**
+     * @param intervals: the given k sorted interval lists
+     * @return: the new sorted interval list
+     */
+    public List<Interval> mergeKSortedIntervalLists(List<List<Interval>> intervals) {
+        return null;
+    }
+}
+
+class Interval {
+    int start, end;
+
+    Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
