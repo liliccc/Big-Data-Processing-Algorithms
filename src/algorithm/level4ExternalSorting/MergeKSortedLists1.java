635233c71b7da8626579866deaa2288ca8f31f99
liliccc <xianj.chen@qq.com>
2022-01-20 16:32:48 +0800

    1.20

---
+++ src/algorithm/level4ExternalSorting/MergeKSortedLists1.java
@@ -0,0 +1,43 @@
+package algorithm.level4ExternalSorting;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MergeKSortedLists1 {
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+    private Comparator<ListNode> comparator = new Comparator<ListNode>() {
+        @Override
+        public int compare(ListNode left, ListNode right) {
+            return left.val - right.val;
+        }
+    };
+    // by using heap (the simplest one) - O(nlogk)
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+        Queue<ListNode> minHeap = new PriorityQueue<>(lists.size(), comparator);
+        for (int i = 0; i < lists.size(); i++) {
+            if (lists.get(i) != null) {
+                minHeap.offer(lists.get(i));
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+            tail.next = node;
+            tail = node;
+            if (node.next != null) {
+                minHeap.offer(node.next);
+            }
+        }
+        return dummy.next;
+    }
+}
+
---
+++ src/algorithm/level4ExternalSorting/MergeKSortedLists3.java
@@ -0,0 +1,13 @@
+package algorithm.level4ExternalSorting;
+
+import java.util.List;
+
+public class MergeKSortedLists3 {
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+    public ListNode mergeKLists(List<ListNode> lists) {
+        return null;
+    }
+}
