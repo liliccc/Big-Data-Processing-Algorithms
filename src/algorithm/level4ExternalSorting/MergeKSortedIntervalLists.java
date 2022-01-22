8d55059dcee456f95074f2e5b947eac5f01cfbfc
liliccc <xianj.chen@qq.com>
2022-01-22 17:20:18 +0800

    1.22

--- src/algorithm/level4ExternalSorting/MergeKSortedIntervalLists.java
+++ src/algorithm/level4ExternalSorting/MergeKSortedIntervalLists.java
@@ -1,5 +1,7 @@
 package algorithm.level4ExternalSorting;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MergeKSortedIntervalLists {
@@ -8,8 +10,30 @@ public class MergeKSortedIntervalLists {
      * @return: the new sorted interval list
      */
     public List<Interval> mergeKSortedIntervalLists(List<List<Interval>> intervals) {
-
-        return null;
+        List<Interval> sortedArray = new ArrayList<>();
+        if (intervals == null || intervals.size() == 0) {
+            return sortedArray;
+        }
+        for (int i = 0; i < intervals.size(); i++) {
+            for (int j = 0; j < intervals.get(i).size(); j++) {
+                sortedArray.add(intervals.get(i).get(j));
+            }
+        }
+        sortedArray.sort((Interval a, Interval b) -> (a.start - b.start));
+        List<Interval> ans = new ArrayList<>();
+        ans.add(sortedArray.get(0));
+        for (int i = 1; i < sortedArray.size(); i++) {
+            int lastNodeIndex = ans.size() - 1;
+            // (1,2), (1,3) => 2 >= 1 => (1,3) || (1,2), (3,4) => 2 < 3 => ( 1, 2) , (3, 4)
+            //error before :ans.get(lastNodeIndex).end > sortedArray.get(i).start
+            if (ans.get(lastNodeIndex).end >= sortedArray.get(i).start) {
+                ans.get(lastNodeIndex).end = Math.max(ans.get(lastNodeIndex).end, sortedArray.get(i).end);
+            }
+            else {
+                ans.add(sortedArray.get(i));
+            }
+        }
+        return ans;
     }
 }
 
